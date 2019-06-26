bb35dda923af0fa6ce619f4aecb4c83c771eefaa
jnq-home <jandroenq@gmail.com>
2019-06-26 19:02:45 -0400

    modified readme file

--- src/components/Maps.js
+++ src/components/Maps.js
@@ -12,7 +12,7 @@ const Maps = (props) => {
     const [error, setError] = useState('')
     const position = props.position
     const topcities = props.numcities
-    console.log(position, "ASI ME LLEGA PARA MAPEAR")
+    //console.log(position, "ASI ME LLEGA PARA MAPEAR")
     
     const mapstyle = { 
         height: props.height, 
@@ -30,12 +30,12 @@ const Maps = (props) => {
     
       async function fecthData(){
       let tempcoords = []
-      console.log(tempcoords, "CORDENADAS TEMPORALES EN ECTHDATA")
-      console.log(props.position, "HOT LOCATION EN FECTCH DATA")
+      //console.log(tempcoords, "CORDENADAS TEMPORALES EN ECTHDATA")
+      //console.log(props.position, "HOT LOCATION EN FECTCH DATA")
       if(props.position !== undefined){
-        console.log(loading, "LOADING EN ENTRADO")
+        //console.log(loading, "LOADING EN ENTRADO")
         isLoading(true)
-        console.log(loading, "LOADING ANTES dE AXIOS")
+        //console.log(loading, "LOADING ANTES dE AXIOS")
       for(let index = 0; index < topcities; index++){
         //console.time("CONSULTA A OSM")
         await axios.get(`https://nominatim.openstreetmap.org/search?format=json&q=${position[index]}`,
@@ -43,7 +43,7 @@ const Maps = (props) => {
           {timeout: 5000}
         )
           .then( result  => {
-            console.log(result.data[0].lat, result.data[0].lon, 'RESPUESTA COORDENADAS')
+            //console.log(result.data[0].lat, result.data[0].lon, 'RESPUESTA COORDENADAS')
             tempcoords.push([result.data[0].lat,  result.data[0].lon])
             //console.timeEnd("CONSULTA A OSM")
           })
@@ -57,11 +57,11 @@ const Maps = (props) => {
           
       
       }
-      console.log(tempcoords, "temcoord antes de pasar a estado")
+      //console.log(tempcoords, "temcoord antes de pasar a estado")
       setCoords(tempcoords)
-      console.log(loading, "LOADING TERMINANDO AXIOS")
+      //console.log(loading, "LOADING TERMINANDO AXIOS")
       isLoading(false)
-      console.log(tempcoords, "coords supuestamente pasadas por tempcoord")
+      //console.log(tempcoords, "coords supuestamente pasadas por tempcoord")
     }
     
   }
@@ -69,7 +69,7 @@ const Maps = (props) => {
 
   }      
   // locationList.map(xxx => fetchData(xxx.name),
-  // console.log(xxx.name, 'no hace')
+  // //console.log(xxx.name, 'no hace')
   // )
       
    ,
